63d29d6899754bf8ec88e5848ae26b6e5287c2b8
bugy <buggygm@gmail.com>
2017-02-05 16:13:03 +0100

    added adapter for animation listening

---
+++ src/main/java/net/buggy/components/animation/AnimationAdapter.java
@@ -0,0 +1,20 @@
+package net.buggy.components.animation;
+
+import android.view.animation.Animation;
+
+
+public abstract class AnimationAdapter implements Animation.AnimationListener {
+    @Override
+    public void onAnimationStart(Animation animation) {
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
