2c2c2285a8ffdd560150f55417ad15f21462bda4
bugy <buggygm@gmail.com>
2017-03-26 16:51:10 +0200

    added helper method for getting app locale

--- src/main/java/net/buggy/components/ViewUtils.java
+++ src/main/java/net/buggy/components/ViewUtils.java
@@ -294,4 +294,15 @@ public class ViewUtils {
 
         return new ContextWrapper(context);
     }
+
+    @SuppressWarnings("deprecation")
+    public static Locale getAppLocale(Context context) {
+        Locale locale;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            locale = context.getResources().getConfiguration().getLocales().get(0);
+        } else {
+            locale = context.getResources().getConfiguration().locale;
+        }
+        return locale;
+    }
 }
